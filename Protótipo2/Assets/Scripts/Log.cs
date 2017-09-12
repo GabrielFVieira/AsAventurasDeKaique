71cd71c5892c65191a813eab95ee84ebbb8dcc79
JackFury360 <gabrielfigueiredov@gmail.com>
2017-09-12 16:41:36 -0300

    log

---
+++ Protótipo2/Assets/Scripts/Log.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour {
+    public GameObject player;
+    public GameObject plataforma;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        plataforma = GameObject.Find("plataformaPlayer");
+}
+	
+	// Update is called once per frame
+	void Update () {
+        if (plataforma.transform.position.y > transform.position.y)
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder + 1;
+        }
+
+        else if (plataforma.transform.position.y < transform.position.y)
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder - 1;
+        }
+
+        else
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder;
+        }
+    }
+}
